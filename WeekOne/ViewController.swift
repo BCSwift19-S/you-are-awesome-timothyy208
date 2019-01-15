e3173a1ba69908f893e7212625cf55e9f88c9f16
Timothy Yang <timothyy208@gmail.com>
2019-01-14 21:21:23 -0500

    added eggplant emoji

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func showButtonPressed(_ sender: UIButton) {
-        print("🍆Button was pressed")
+        print("🍆Button was pressed🍆")
     }
     
 }
