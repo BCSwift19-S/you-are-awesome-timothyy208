fff4e69cf3f24f8b1c0cba97e404002153ce8d1c
Timothy Yang <timothyy208@gmail.com>
2019-01-14 21:20:24 -0500

    added button

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Timothy Yang on 1/14/19.
+//  Copyright © 2019 Timothy Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("🤮viewDidLoad just executed!")
+        
+    }
+    
+    
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+        print("🍆Button was pressed")
+    }
+    
+}
+
