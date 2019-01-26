5dd1ed3a50eaed05c917aab3247282297a84e46a
Timothy Yang <timothyy208@gmail.com>
2019-01-26 15:14:36 -0500

    2.13

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -32,7 +32,14 @@ class ViewController: UIViewController {
                         "You are tremendous!",
                         "You've got the design skills of Jony Ive!",
                         "I can't wait to download your app!"]
-        messageLabel.text = messages.randomElement()!
+        
+        var newIndex = -1
+        repeat{
+            newIndex = Int.random(in: 0..<messages.count)
+        } while index == newIndex
+        index = newIndex
+        messageLabel.text = messages[index]
+        
 //        messageLabel.text = messages[index]
 //        index += 1
 //        if index == messages.count{
